e3d037cc4e78d8ccce4a86af56412eeddd6bba59
joerverson.santos <joerverson.santos@gmail.com>
2018-08-14 13:04:08 -0300

    alterado o ip da maguina

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import game from './app';
 import io from 'socket.io-client/lib';
 
-const socket = io('http://192.168.0.23:3000');
+const socket = io('http://10.0.60.142:3000');
 
 //startand as scenes
 game.remote = socket;
